4942d1bce1d87be1d12ce4cebda9d27e80ff14fc
Ajay Jat <jatajay004@gmail.com>
2019-12-23 12:53:19 +0530

    uploading files

---
+++ src/components/CardList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Cards from './Cards';
+//import Values from './Values';
+
+const CardList=({Values}) => {
+	return(
+		<div>
+          {
+          	Values.map((users,i)=>{
+          	                    	return(
+          	                    		<Cards
+          	                    		key={i} 
+          	                    		id={Values[i].id} 
+          	                    		name={Values[i].name} 
+          	                    		email={Values[i].email} 
+          	                    		/>
+          	          
+          	          
+          	                    		  );
+          	                      }
+          	           )
+                                   
+           }
+         </div>
+
+          );
+
+}
+
+export default CardList;
---
+++ src/components/Searchbox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const searchbox=({searchchange,searchfield})=>{
+return(
+	<div>
+	<input
+	 className='tc bg-lightest-blue ma3 pa3 dib' 
+	 type='search' 
+	 placeholder='Search Robots'
+	 onChange={searchchange}
+	 />
+
+	 </div>
+	 );
+
+}
+export default searchbox;
